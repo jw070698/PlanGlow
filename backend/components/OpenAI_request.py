dc33aafda0f572d4f931b1bdcaf473b079b06c00
jw070698 <jiwonchun@tamu.edu>
2024-08-26 22:48:30 -0500

    edit prompt

--- backend/components/OpenAI_request.py
+++ backend/components/OpenAI_request.py
@@ -15,7 +15,7 @@ class ChatApp:
             If user asks create study plans or change plan, try to suggest best possible fit in terms of time limitation, type of resources, their knowledge base.\
             Show the overview of the generated plans, and then detailed daily plans. \
             The recommended resources should be with accessible and available links, and the one that came out before shouldn't come out after that. Don't use same resources for the all plan.\
-            In addition, match resources with the each day topic, and you can recommend multiple resources each day, based on user's available time per day.\
+            In addition, match resources with the each day topic, and you can recommend multiple resources each day if user's available time per day >= 3.\
             The available time per day means users can study input hours per a day. Make plan matches with this input time.\
             Make sure: 1 week have to be consisted of 5 days, 1 month have to be consisted of 4 weeks.\
             For example, if users want to study 4 weeks 4 days available time per day = 2 hours, then the plan should be 4 weeks and 4 days with available time of 2 hours daily.\
