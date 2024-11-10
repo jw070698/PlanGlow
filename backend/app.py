738ac0ef47b6c98f6f018a8e8fd7c5e15b32cc12
jw070698 <jiwonchun@tamu.edu>
2024-11-09 22:10:48 -0600

    fix bug

--- backend/app.py
+++ backend/app.py
@@ -76,19 +76,19 @@ def extract_topic(user_message):
 
 @app.post("/response")
 async def generate_response(request: MessageRequest):
-    if not request.user_message or not request.participantId:
-        return {"response": "user_message and participantId are required fields"}, 422
-    if request.user_message:
-        participantId = request.participantId
-        response_text = chat_app.chat(request.user_message)
-        store_messages(participantId, request.user_message, response_text)  # Store user message & study plan
-    elif request.user_input:
-        participantId = request.participantId
-        response_text = chat_app.chat(request.user_input) 
-        store_messages(participantId, request.user_input, response_text) # Store user input & study plan
-    else:
-        response_text = 'No message'
-    return {"response": response_text}
+    
+    #if request.user_message:
+    #    participantId = request.participantId
+    #    response_text = chat_app.chat(request.user_message)
+    #    store_messages(participantId, request.user_message, response_text)  # Store user message & study plan
+    #elif request.user_input:
+    #    participantId = request.participantId
+    #    response_text = chat_app.chat(request.user_input) 
+    #    store_messages(participantId, request.user_input, response_text) # Store user input & study plan
+    #else:
+    #    response_text = 'No message'
+    
+    return {"response": chat_app.chat(request.user_message)}
 
 @app.post("/info")
 async def generate_info_response(request: InfoRequest):
